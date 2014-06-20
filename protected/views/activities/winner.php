3a2c209d60480a7edeeb61ec8ba80c46cedea58f
fuel <gif@fuel-it-up.com>
2014-06-20 21:49:26 +0800

    updat

--- protected/views/activities/winner.php
+++ protected/views/activities/winner.php
@@ -5,7 +5,7 @@
         <div class="act_wordtit">第三周</div>
         <div class="act_videolist cs-clear">
             <!--  -->
-            <div class="act_videoitem" onclick="viewVideo('/malibu/activities/video.html?url=4sSmOuna-ZMHWGaDszV-vQ__')">
+            <div class="act_videoitem" onclick="viewVideo('/2/activities/video.html?url=4sSmOuna-ZMHWGaDszV-vQ__')">
                 <div class="act_video"><img src="<?=Yii::app()->baseUrl?>/img/act_demo.jpg" /></div>
                 <div class="act_videoinfo">
                     <p class="act_infotiem">2014.5.30</p>
@@ -13,7 +13,7 @@
                 </div>
             </div>
             <!--  -->
-            <div class="act_videoitem" onclick="viewVideo('/malibu/activities/video.html?url=4sSmOuna-ZMHWGaDszV-vQ__')">
+            <div class="act_videoitem" onclick="viewVideo('/2/activities/video.html?url=4sSmOuna-ZMHWGaDszV-vQ__')">
                 <div class="act_video"><img src="<?=Yii::app()->baseUrl?>/img/act_demo.jpg" /></div>
                 <div class="act_videoinfo">
                     <p class="act_infotiem">2014.5.30</p>
@@ -21,7 +21,7 @@
                 </div>
             </div>
             <!--  -->
-            <div class="act_videoitem" onclick="viewVideo('/malibu/activities/video.html?url=4sSmOuna-ZMHWGaDszV-vQ__')">
+            <div class="act_videoitem" onclick="viewVideo('/2/activities/video.html?url=4sSmOuna-ZMHWGaDszV-vQ__')">
                 <div class="act_video"><img src="<?=Yii::app()->baseUrl?>/img/act_demo.jpg" /></div>
                 <div class="act_videoinfo">
                     <p class="act_infotiem">2014.5.30</p>
@@ -30,7 +30,7 @@
             </div>
             <!--  -->
         </div>
-        <div class="act_tips">所有获奖者请联络Malibuband马利宝天团官方微博</div>
+        <div class="act_tips">所有获奖者请联络<span class="en">Malibuband</span>马利宝天团官方微博</div>
     </div>
     <!-- mod -->
     <!-- mod -->
@@ -39,14 +39,14 @@
         <div class="act_videolist cs-clear">
             <!--  -->
             <div class="act_videoitem">
-                <div class="act_video" onclick="viewVideo('/malibu/activities/video.html?url=4sSmOuna-ZMHWGaDszV-vQ__')"><img src="<?=Yii::app()->baseUrl?>/img/act_demo.jpg" /></div>
+                <div class="act_video" onclick="viewVideo('/2/activities/video.html?url=4sSmOuna-ZMHWGaDszV-vQ__')"><img src="<?=Yii::app()->baseUrl?>/img/act_demo.jpg" /></div>
                 <div class="act_videoinfo">
                     <p class="act_infotiem">2014.5.30</p>
                     <div class="act_infouser">@随波逐流 <span><img src="<?=Yii::app()->baseUrl.'/style/fontend/'?>img/act_weiboicon.png" /></span></div>
                 </div>
             </div>
             <!--  -->
-            <div class="act_videoitem" onclick="viewVideo('/malibu/activities/video.html?url=4sSmOuna-ZMHWGaDszV-vQ__')">
+            <div class="act_videoitem" onclick="viewVideo('/2/activities/video.html?url=4sSmOuna-ZMHWGaDszV-vQ__')">
                 <div class="act_video"><img src="<?=Yii::app()->baseUrl?>/img/act_demo.jpg" /></div>
                 <div class="act_videoinfo">
                     <p class="act_infotiem">2014.5.30</p>
@@ -54,7 +54,7 @@
                 </div>
             </div>
             <!--  -->
-            <div class="act_videoitem" onclick="viewVideo('/malibu/activities/video.html?url=4sSmOuna-ZMHWGaDszV-vQ__')">
+            <div class="act_videoitem" onclick="viewVideo('/2/activities/video.html?url=4sSmOuna-ZMHWGaDszV-vQ__')">
                 <div class="act_video"><img src="<?=Yii::app()->baseUrl?>/img/act_demo.jpg" /></div>
                 <div class="act_videoinfo">
                     <p class="act_infotiem">2014.5.30</p>
@@ -63,7 +63,7 @@
             </div>
             <!--  -->
         </div>
-        <div class="act_tips">所有获奖者请联络Malibuband马利宝天团官方微博</div>
+        <div class="act_tips">所有获奖者请联络<span class="en">Malibuband</span>马利宝天团官方微博</div>
     </div>
     <!-- mod -->
 
